68e170f6203fe31fd5baff18ab451410b6001ed6
Jorge Rodriguez <dalerojo2000@gmail.com>
2023-09-17 18:48:13 -0300

    avances proyecto react parte 2

--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Nav = (props) => {
     return (
     <>
        <nav id="botonera_nav">
         
         <ul className="holder">
-            <li><Link to= "/">Inicio</Link></li>
-            <li><Link to= "/Servicios">Servicios</Link></li>
-            <li><Link to= "/Equipos">Equipos</Link></li>
-            <li><Link to= "/Cotizador">Cotizador</Link></li>
-            <li><Link to= "/Nosotros">Nosotros</Link></li>
+            <li><NavLink to= "/" className={({isActive})=> isActive?"activo" : undefined}>Inicio</NavLink></li>
+            <li><NavLink to= "/Servicios" className={({isActive})=> isActive?"activo" : undefined}>Servicios</NavLink></li>
+            <li><NavLink to= "/Equipos" className={({isActive})=> isActive?"activo" : undefined}>Equipos</NavLink></li>
+            <li><NavLink to= "/Cotizador" className={({isActive})=> isActive?"activo" : undefined}>Cotizador</NavLink></li>
+            <li><NavLink to= "/Nosotros" className={({isActive})=> isActive?"activo" : undefined}>Nosotros</NavLink></li>
 
 
         </ul>
